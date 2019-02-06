ece0ef5428e67dd366e5758c33cc8dece2628496
sabushaban@ucdavis.edu <Cutesara1>
2019-02-06 11:59:49 -0800

    Studio 2

--- studio1/script.js
+++ studio1/script.js
@@ -1,20 +1,21 @@
-console.log('reading js');
-
 'use strict';
+console.log('reading js');
 
 document.querySelector('#s').addEventListener('click', processForm);
 
 function processForm(evt) {
 
-  var breed = document.querySelector('#breed').value;
+  var firstL = document.querySelector('#firstL').value;
 
 
-  var favMovie = document.querySelector('#favMovie').value;
+  var last = document.querySelector('#last').value;
 
+  var age = document.querySelector('#age').value;
 
   var myMsg = document.querySelector('#myMsg');
 
   myMsg.innerHTML = ('Hi, my favorite breed is ' + breed + ' I love everything about' + breed + favMovie + ' too!');
 
   evt.preventDefault();
+
 }
