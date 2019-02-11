ad3899e57a9791c5cf5188753e76a9b696071c6e
Sara AbuShaban <SaraAbuShaban@Saras-MacBook-Pro.local>
2019-02-11 06:23:12 -0800

    click

--- studio2/script.js
+++ studio2/script.js
@@ -1,26 +1,27 @@
 'use strict';
 
 var phone = document.querySelector('#phone');
-var lipstick = document.querySelector('#lipstick');
+var lipstick1 = document.querySelector('#lipstick1');
 var usb = document.querySelector('#usb');
 
 
 var close = document.querySelector('#close')
+var lipstickclose = document.querySelector('#lipstickclose')
 var overlay = document.querySelector('#overlay')
-
+var overlay1 = document.querySelector('#overlay')
 
 phone.addEventListener('click', function() {
-  overlay.style.display='block';
+  overlay.style.display = 'block';
 })
 
-close.addEventListener('click', function(){
-
+close.addEventListener('click', function() {
+  overlay.style.display = 'none';
 })
 
 lipstick.addEventListener('click', function() {
-  overlay.style.display='block';
+  overlay1.style.display = 'block';
 })
 
-close.addEventListener('click', function(){
-
+close.addEventListener('click', function() {
+  overlay1.style.display = 'none';
 })
