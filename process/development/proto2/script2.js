4e91024a8e0bb993690aa06cfdec688d17351916
Sara Abushaban <sjabusha@scc2102-12.ucdavis.edu>
2019-03-13 10:24:17 -0700

    edit

--- process/development/proto2/script2.js
+++ process/development/proto2/script2.js
@@ -17,13 +17,13 @@ console.log("reading js");
 
 var input
 
-var threshold = 0.1;
+var threshold = .1;
 var cutoff = 0;
-var decayRate = 0.95;
+var decayRate = 0.5;
 
 var day =document.querySelector('#day');
 function setup() {
-  createCanvas(710, 200);
+  createCanvas(910, 895);
   background(255);
 
   // Create an Audio input
@@ -38,19 +38,21 @@ function draw() {
   // If the volume > threshold + cutoff, a rect is drawn at a random location.
   // The louder the volume, the larger the rectangle.
   if (volume > threshold + cutoff) {
-    stroke(0);
-    fill(0, 100);
-    rect(random(40, width), random(height), volume*50, volume*50);
+    // stroke(0);
+    fill(random(20),random(200),100);
+    rect(random(40, width*20), random(height), volume*50, volume*50);
+    fill(random(200),random(20),100);
+    ellipse(random(40, width*20), random(height), volume*90, volume*90);
 
     // increase the cutoff
-    cutoff = 0.5;
+    cutoff = 0;
   }
 
   //
-  cutoff = cutoff * decayRate;
+  // cutoff = cutoff * decayRate;
 
   // Graph the overall potential volume, w/ a line at the threshold
-  var y = map(volume, 0, 1, height, 0);
+  var y = map(volume, 0, .2, height, 0);
   var ythreshold = map(threshold + cutoff, 0, 1, height, 0);
 
   noStroke();
